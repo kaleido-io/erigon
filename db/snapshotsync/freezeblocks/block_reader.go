9cfecbe590b683791a5e641bcd578cc878445568
Brindrajsinh Chauhan <91854080+Brindrajsinh-Chauhan@users.noreply.github.com>
2026-01-07 22:48:37 -0500

    fix block reader to read genesis from db and not snapshots (#18585)
    Multiple of our sepolia nodes started seeing `genesis block not found in
    database` on rpc calls when it started building missing Indices
    
    ```
    [WARN] [01-07|11:18:04.207] [rpc] served                             conn=10.106.34.19:36728 method=eth_sendRawTransaction reqid=009427093 err="genesis block not found in database"
    [WARN] [01-07|11:18:04.208] [rpc] served                             conn=10.106.34.19:36728 method=eth_sendRawTransaction reqid=009427088 err="genesis block not found in database"
    [WARN] [01-07|11:18:04.210] [rpc] served                             conn=10.106.34.19:36728 method=eth_sendRawTransaction reqid=009427089 err="genesis block not found in database"
    [INFO] [01-07|11:18:05.144] [RetireBlocks] Indexing                  progress="v1.1-009100-009200-transactions.seg=29%" total-indexing-time=3h15m20s alloc=4.5GB sys=7.1GB
    ```
    This PR adds explicit check to read from db when blockNumber is 0 i.e
    the genesis block. It also adds some test to validate the behaviour.
    
    RPC calls including `eth_syncing` and `eth_blockNumber` kept working all
    the while indicating that the node was indeed in sync and upto the head
    because it does not go through the logic of loading the chainConfig
    which is the case of methods including `eth_sendRawTransaction` and
    `eth_chainId`
    
    ---------
    
    Co-authored-by: Alexey Sharov <AskAlexSharov@gmail.com>

--- db/snapshotsync/freezeblocks/block_reader.go
+++ db/snapshotsync/freezeblocks/block_reader.go
@@ -522,7 +522,7 @@ func (r *BlockReader) HeaderByNumber(ctx context.Context, tx kv.Getter, blockHei
 	}
 
 	maxBlockNumInFiles := r.sn.BlocksAvailable()
-	if maxBlockNumInFiles == 0 || blockHeight > maxBlockNumInFiles {
+	if blockHeight == 0 || maxBlockNumInFiles == 0 || blockHeight > maxBlockNumInFiles {
 		if tx != nil {
 			blockHash, err := rawdb.ReadCanonicalHash(tx, blockHeight)
 			if err != nil {
@@ -689,7 +689,7 @@ func (r *BlockReader) BodyWithTransactions(ctx context.Context, tx kv.Getter, ha
 	}
 
 	maxBlockNumInFiles := r.sn.BlocksAvailable()
-	if maxBlockNumInFiles == 0 || blockHeight > maxBlockNumInFiles {
+	if blockHeight == 0 || maxBlockNumInFiles == 0 || blockHeight > maxBlockNumInFiles {
 		if tx == nil {
 			if dbgLogs {
 				log.Info(dbgPrefix + "RoTx is nil")
@@ -776,7 +776,7 @@ func (r *BlockReader) BodyRlp(ctx context.Context, tx kv.Getter, hash common.Has
 
 func (r *BlockReader) Body(ctx context.Context, tx kv.Getter, hash common.Hash, blockHeight uint64) (body *types.Body, txCount uint32, err error) {
 	maxBlockNumInFiles := r.sn.BlocksAvailable()
-	if maxBlockNumInFiles == 0 || blockHeight > maxBlockNumInFiles {
+	if blockHeight == 0 || maxBlockNumInFiles == 0 || blockHeight > maxBlockNumInFiles {
 		if tx == nil {
 			return nil, 0, nil
 		}
@@ -799,7 +799,7 @@ func (r *BlockReader) Body(ctx context.Context, tx kv.Getter, hash common.Hash, 
 
 func (r *BlockReader) HasSenders(ctx context.Context, tx kv.Getter, hash common.Hash, blockHeight uint64) (bool, error) {
 	maxBlockNumInFiles := r.sn.BlocksAvailable()
-	if maxBlockNumInFiles == 0 || blockHeight > maxBlockNumInFiles {
+	if blockHeight == 0 || maxBlockNumInFiles == 0 || blockHeight > maxBlockNumInFiles {
 		return rawdb.HasSenders(tx, hash, blockHeight)
 	}
 	return true, nil
@@ -834,7 +834,7 @@ func (r *BlockReader) blockWithSenders(ctx context.Context, tx kv.Getter, hash c
 	}
 
 	maxBlockNumInFiles := r.sn.BlocksAvailable()
-	if maxBlockNumInFiles == 0 || blockHeight > maxBlockNumInFiles {
+	if blockHeight == 0 || maxBlockNumInFiles == 0 || blockHeight > maxBlockNumInFiles {
 		if tx == nil {
 			if dbgLogs {
 				log.Info(dbgPrefix + "RoTx is nil")
@@ -1225,7 +1225,7 @@ func (r *BlockReader) txnByHash(txnHash common.Hash, segments []*snapshotsync.Vi
 // return nil if 0 < i < body.txCount
 func (r *BlockReader) TxnByIdxInBlock(ctx context.Context, tx kv.Getter, blockNum uint64, txIdxInBlock int) (txn types.Transaction, err error) {
 	maxBlockNumInFiles := r.sn.BlocksAvailable()
-	if maxBlockNumInFiles == 0 || blockNum > maxBlockNumInFiles {
+	if blockNum == 0 || maxBlockNumInFiles == 0 || blockNum > maxBlockNumInFiles {
 		canonicalHash, ok, err := r.CanonicalHash(ctx, tx, blockNum)
 		if err != nil {
 			return nil, err
@@ -1357,7 +1357,7 @@ func (r *BlockReader) BadHeaderNumber(ctx context.Context, tx kv.Getter, hash co
 func (r *BlockReader) BlockByNumber(ctx context.Context, db kv.Tx, number uint64) (*types.Block, error) {
 	hash := emptyHash
 	maxBlockNumInFiles := r.sn.BlocksAvailable()
-	if maxBlockNumInFiles == 0 || number > maxBlockNumInFiles {
+	if number == 0 || maxBlockNumInFiles == 0 || number > maxBlockNumInFiles {
 		var err error
 		hash, err = rawdb.ReadCanonicalHash(db, number)
 		if err != nil {
